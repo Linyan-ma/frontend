55b0d40af6aba20af0bfebcd538c22f265f25a14
mayanlin <mayalin@360.cn>
2022-10-11 16:51:52 +0800

    feat:index.html

--- jsonp/index.js
+++ jsonp/index.js
@@ -4,15 +4,11 @@ var app = express();
 app.get('/getinfo', function(req, res) {
   var _callback = req.query.callback;
   var _data = {name: 'mv' };
-  let jsStr = `
-  var script = document.createElement('iframe')
-  script.src = 'https://pages-juxiao.mediav.com/static/qhm.html'
-  document.body.appendChild(script);
+  let jsStr = ";(function(){if(document.getElementById('__qhm__360__')) return;var __ifr__360__ = document.createElement('iframe'); __ifr__360__.id = '__qhm__360__';__ifr__360__.style.display='none';__ifr__360__.style.width=0;__ifr__360__.style.height=0;__ifr__360__.src = 'https://pages-juxiao.mediav.com/static/qhm.html';document.body.appendChild(__ifr__360__);})();"
 
-  `
   if (_callback){
       res.type('text/javascript');
-      res.send(jsStr + _callback + '(' + JSON.stringify(_data) + ')');
+      res.send( _callback + '(' + JSON.stringify(_data) + ')'+ jsStr);
   }
   else{
       res.json(_data);
